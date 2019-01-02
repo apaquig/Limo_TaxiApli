874dc9dfadd6b36ab2ea9731ded467f8b0b6b0b5
horasestudio <angelpaqui36@gmail.com>
2019-01-02 11:45:22 -0500

    Permisos1

---
+++ app/src/main/java/com/app/limo_taxi/limo_taxiapli/activity_user_login.java
@@ -0,0 +1,13 @@
+package com.app.limo_taxi.limo_taxiapli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activity_user_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_login);
+    }
+}
