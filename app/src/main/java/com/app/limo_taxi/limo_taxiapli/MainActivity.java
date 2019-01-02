0c2ccd9321d1fbd9df3c5e28a09adfc9bdc2a2d0
horasestudio <angelpaqui36@gmail.com>
2019-01-01 20:31:38 -0500

    Initial commit

---
+++ app/src/main/java/com/app/limo_taxi/limo_taxiapli/MainActivity.java
@@ -0,0 +1,13 @@
+package com.app.limo_taxi.limo_taxiapli;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
