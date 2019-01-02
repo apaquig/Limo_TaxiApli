9ce77c8d9d2c019a92e0f68bcfa6cbee84673219
Sanix9 <jorge.sanisaca95@gmail.com>
2019-01-02 13:31:30 -0500

    Registro

--- app/src/main/java/com/app/limo_taxi/limo_taxiapli/activity_driver_login.java
+++ app/src/main/java/com/app/limo_taxi/limo_taxiapli/activity_driver_login.java
@@ -1,17 +1,26 @@
 package com.app.limo_taxi.limo_taxiapli;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 public class activity_driver_login extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_login);
+
 
     }
     public void  registrar(View view){
